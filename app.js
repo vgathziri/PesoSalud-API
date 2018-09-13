f7b323d53f8d635c71dea61ce37ceeeffeb0dd03
Jacob Nuno <jacobcnda@gmail.com>
2018-09-13 08:44:51 -0500

    eslint configuration added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('This is Home!');
 });
 
 app.listen(3000, () => {
