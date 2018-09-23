b38037a07ae61178161d3d0172beddb3807ca543
Jacob Nuno <jacobcnda@gmail.com>
2018-09-23 12:13:25 -0500

    mvc base

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,49 @@
-exports.getAll = () => {
-  console.log('lala');
-};
+class UserCtrl {
+  constructor() {
+    // User data temporary hardcoded
+    this.users = [
+      {
+        id: 1,
+        name: 'felipe',
+      },
+      {
+        id: 2,
+        name: 'eduardo',
+      },
+      {
+        id: 3,
+        name: 'juan',
+      },
+    ];
+
+    // Binding this to not loose context on router
+    this.getAll = this.getAll.bind(this);
+    this.get = this.get.bind(this);
+    this.create = this.create.bind(this);
+  }
+
+  getAll(req, res) {
+    return res.status(200).send({ data: this.users });
+  }
+
+  get(req, res) {
+    const data = this.data.find(el => el.id === Number(req.params.userId));
+
+    res.status(200).send(data);
+  }
+
+  create(req, res) {
+    const lastId = this.data[this.data.length - 1].id;
+    const data = {
+      id: lastId + 1,
+      name: req.body.name,
+      email: req.body.email,
+    };
+
+    this.data.push(data);
+
+    res.status(201).send(data);
+  }
+}
+
+module.exports = new UserCtrl();
