3fdbb50fc56ac57bc8721d7377fd3ccf93b72094
Carlos Orozco <ceoe1996@hotmail.com>
2018-09-23 14:51:01 -0500

    Adding Appointments and MedicalRecord Endpoints

---
+++ controllers/appointment.js
@@ -0,0 +1,65 @@
+class AppointmentCtrl {
+  constructor() {
+    this.data = [
+      {
+        id: 1,
+        userID: 1,
+        serviceID: 1,
+        placeID: 1,
+        date: '20180910',
+        status: 'Agendado',
+      },
+      {
+        id: 2,
+        userID: 2,
+        serviceID: 2,
+        placeID: 2,
+        date: '20180911',
+        status: 'Cancelado',
+      },
+    ];
+
+    this.getByDate = this.getByDate.bind(this);
+    this.getByUserID = this.getByUserID.bind(this);
+    this.getByPlaceID = this.getByPlaceID.bind(this);
+    this.create = this.create.bind(this);
+    this.put = this.put.bind(this);
+  }
+
+  getByDate(req, res) {
+    res.send(this.data);
+  }
+
+  getByUserID(req, res) {
+    res.send(this.data);
+  }
+
+  getByPlaceID(req, res) {
+    res.send(this.data);
+  }
+
+  create(req, res) {
+    const data = {
+      userID: req.body.userID,
+      serviceID: req.body.serviceID,
+      placeID: req.body.placeID,
+      date: req.body.date,
+      status: req.body.status,
+    };
+
+    this.data.push(data);
+
+    res.status(201).send(data);
+  }
+
+  edit(req, res) {
+    const json = {
+      data: this.data,
+      message: 'Item updated',
+    };
+
+    res.status(200).send(json);
+  }
+}
+
+module.exports = new AppointmentCtrl();
---
+++ controllers/medicalRecords.js
@@ -0,0 +1,79 @@
+class MedicalRecordsCtrl {
+  constructor() {
+    this.data = [
+      {
+        id: 1,
+        userID: 1,
+        appointmentID: 1,
+        serviceID: 1,
+        weight: 71.5,
+        bust: 70.1,
+        waistline: 65.0,
+        hip: 67.8,
+        chest: 75.4,
+        abdomen: 81.2,
+        dietID: 1,
+        symptom: 'Gripa',
+        comments: 'Paciente Nuevo',
+        initialHighAbdomen: 77.3,
+        finalHighAbdomen: 75.8,
+        initialMediumAbdomen: 77.3,
+        finalMediumAbdomen: 75.8,
+        initialLowAbdomen: 77.3,
+        finalLowAbdomen: 75.8,
+      },
+    ];
+
+    this.getByUserID = this.getByUserID.bind(this);
+    this.getByAppointmentID = this.getByAppointmentID.bind(this);
+    this.create = this.create.bind(this);
+    this.edit = this.edit.bind(this);
+  }
+
+  getByDate(req, res) {
+    res.send(this.data);
+  }
+
+  getByAppointmentID(req, res) {
+    res.send(this.data);
+  }
+
+  create(req, res) {
+    const data = {
+      id: req.body.id,
+      userID: req.body.userID,
+      appointmentID: req.body.appointmentID,
+      serviceID: req.body.serviceID,
+      weight: req.body.weight,
+      bust: req.body.bust,
+      waistline: req.body.waistline,
+      hip: req.body.hip,
+      chest: req.body.chest,
+      abdomen: req.body.abdomen,
+      dietID: req.body.dietID,
+      symptom: req.body.symptom,
+      comments: req.body.comments,
+      initialHighAbdomen: req.body.initialHighAbdomen,
+      finalHighAbdomen: req.body.finalHighAbdomen,
+      initialMediumAbdomen: req.body.initialMediumAbdomen,
+      finalMediumAbdomen: req.body.finalMediumAbdomen,
+      initialLowAbdomen: req.body.initialLowAbdomen,
+      finalLowAbdomen: req.body.finalLowAbdomen,
+    };
+
+    this.data.push(data);
+
+    res.status(201).send(data);
+  }
+
+  edit(req, res) {
+    const json = {
+      data: this.data,
+      message: 'Item updated',
+    };
+
+    res.status(201).send(json);
+  }
+}
+
+module.exports = new MedicalRecordsCtrl();
