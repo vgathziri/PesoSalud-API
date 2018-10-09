a70f2fd3c8670d87bfbd4bac4d0a3e72c8dfb108
Carlos Orozco <ceoe1996@hotmail.com>
2018-10-08 23:05:29 -0500

    Connect Places Model to DB and Controller to Model. Issues #69 #70

--- routes/places.js
+++ routes/places.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const { placesCtrl } = require('../controllers');
+const middlewares = require('../middlewares');
 
 router.get('/', placesCtrl.getAll);
-router.post('/', placesCtrl.create);
+router.post('/', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    Body: {
+      Name: 'required',
+      PlaceType: 'required',
+      Active: 'required',
+    },
+  });
+}, placesCtrl.create);
 router.put('/:ID', placesCtrl.edit);
 
 module.exports = router;
